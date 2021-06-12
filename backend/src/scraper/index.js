980dadda7e9b765e540f8daa24b3760227142b80
alisher <aajk2008@gmail.com>
2021-06-11 22:52:55 -0500

    fetchHtml function complete with test

--- backend/src/scraper/index.js
+++ backend/src/scraper/index.js
@@ -1,3 +1,58 @@
+const cheerio = require("cheerio");
+const axios = require("axios").default;
+
+const fethHtml = async url => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch {
+    console.error(`ERROR: An error occurred while trying to fetch the URL: ${url}`);
+  }
+};
+
+const extractRatingNumber
+
+const extractDeal = selector => {
+  const username = selector
+    .find("span[class='italic font-18 black notranslate']")
+    .text()
+    .trim();
+
+  const rating = selector
+    .find("div[class='rating-static visible-xs pad-none margin-none']")
+    .attr('class');
+  rating.
+
+  return {
+    username,
+
+  };
+};
+
+const scrapDealerSite = async () => {
+  const steamUrl = 'https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page1/?filter=ONLY_POSITIVE';
+  const html = await fethHtml(steamUrl);
+  const $ = cheerio.load(html);
+
+  const searchResults = $("body").find("div.review-entry");
+
+  const reviews = searchResults
+    .map((idx, el) => {
+      const elementSelector = $(el);
+      return extractDeal(elementSelector);
+    })
+    .get();
+
+  return reviews;
+};
+
+// scrapDealerSite().then((html)=>{
+//     console.log(html);
+
+// });
+
 
 module.exports = {
+    fethHtml:fethHtml,
+    scrapDealerSite:scrapDealerSite
 };
--- backend/test/scraper.js
+++ backend/test/scraper.js
@@ -3,16 +3,10 @@ const expect = chai.expect
 
 const scraper = require('../src/scraper')
 
-describe("scrape function()", () => {
-
-	it("should be pingable", ()=> {
+describe("fetchHtml function", () => {
+	xit("should return non empty result from webscraper website", async () => {
+		const html = await scraper.fethHtml("https://webscraper.io/test-sites/e-commerce/allinone");
+		expect(html).to.be.not.empty;
 	})
-
-	it("should return non empty result from website", ()=> {
-	})
-
-	it("should have json as output", ()=> {
-	})
-
-
 })
+
