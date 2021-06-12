b9363ece6784b28dbfad3c0cc07761290adc966e
alisher <aajk2008@gmail.com>
2021-06-11 21:36:15 -0500

    initial setup from scraper with some unit tests

---
+++ backend/test/scraper.js
@@ -0,0 +1,18 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const scraper = require('../src/scraper')
+
+describe("scrape function()", () => {
+
+	it("should be pingable", ()=> {
+	})
+
+	it("should return non empty result from website", ()=> {
+	})
+
+	it("should have json as output", ()=> {
+	})
+
+
+})
